9edd0cf08ade203886f1360fc4796e3fcfb278f4
Mark5013 <mark1mcennis@gmail.com>
2026-08-10 18:42:26 -0400

    Accept URL argument

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    if env::args().len() != 2 {
+        eprintln!("Incorrect number of arguments. Please provide only one argument.");
+        std::process::exit(1);
+    }
+    let arg = env::args().nth(1).unwrap();
+    println!("{arg}");
 }

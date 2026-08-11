685ba6988b4e1e3b70cb64b04f59bbef0cbcf063
Mark5013 <mark1mcennis@gmail.com>
2026-08-11 19:46:30 -0400

    Report request latency

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::env;
 use reqwest::blocking::Client;
-use std::time::Duration;
+use std::env;
+use std::time::{Duration, Instant};
 
 fn main() {
     if env::args().len() != 2 {
@@ -16,18 +16,31 @@ fn main() {
             std::process::exit(1);
         }
     };
+    let started = Instant::now();
     let result = client.get(&arg).send();
+    let elapsed_time_ms = started.elapsed().as_millis();
     match result {
         Ok(response) => {
             if response.status().is_success() {
-                println!("Request successful for {arg} with a status code of {}", response.status());
+                println!(
+                    "Request successful for {arg} with a status code of {} (took {} ms)",
+                    response.status(),
+                    elapsed_time_ms
+                );
             } else {
-                eprintln!("Request failed for {arg} with status: {}", response.status());
+                eprintln!(
+                    "Request failed for {arg} with status: {} (took {} ms)",
+                    response.status(),
+                    elapsed_time_ms
+                );
                 std::process::exit(1);
             }
         }
         Err(e) => {
-            eprintln!("Error making request for {arg} with error of: {}", e);
+            eprintln!(
+                "Error making request for {arg} with error of: {} (took {} ms)",
+                e, elapsed_time_ms
+            );
             std::process::exit(1);
         }
     }
